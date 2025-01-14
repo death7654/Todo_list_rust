37199c0496d57b6dacd5754820bea91993b4ec30
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-01-14 21:42:42 +0530

    if control to flow control flow

--- src/main.rs
+++ src/main.rs
@@ -43,26 +43,27 @@ fn main() {
         let split_command: Vec<&str> = command.split(" ").map(|x| x.trim()).collect::<Vec<&str>>();
 
         //all commands
-        match split_command[0] {
-            "add" => {}
-            "remove" => {}
-            "edit" => {}
-            "complete" => {}
-            "change_list" => {
+        let request = split_command[0].to_owned();
+        match request {
+            _ if request.contains("add") => {}
+            _ if request.contains("remove") => {}
+            _ if request.contains("edit") => {}
+            _ if request.contains("complete") => {}
+            _ if request.contains("change_list") => {
                 let picked_file = all_files[split_command[1].parse::<usize>().unwrap()].clone();
                 if change_list(&picked_file) {
                     file = picked_file;
                 }
             }
-            "create_list" => {
+            _ if request.contains("create_list") => {
                 create_list((split_command[1..]).to_vec());
             }
-            "delete_list" => {
+            _ if request.contains("delete_list") => {
                 delete_list(split_command[1].parse().unwrap());
             }
-            "edit_list" => edit_list(split_command[1].parse().unwrap(), &file),
-            "lists" => list_files(),
-            "quit" => break,
+            _ if request.contains("edit_list") => edit_list(split_command[1].parse().unwrap(), &split_command[2..]),
+            _ if request.contains("lists") => list_files(),
+            _ if request.contains("quit") => break,
             _ => {
                 println!("Invalid Command")
             }
@@ -89,17 +90,21 @@ fn create_list(vector: Vec<&str>) {
 }
 fn delete_list(index: usize) {
     let files = vector_files();
-    let output = fs::remove_file("C:\\todo".to_owned() + &files[index]);
+    let delete = &files[index];
+    let output = fs::remove_file(delete);
     match output {
-        Ok(_) => println!("{}, has been successfully deleted", &files[index]),
+        Ok(_) => println!("{} has been successfully deleted", &files[index]),
         Err(e) => println!("The file has not been deleted \n\n{e}"),
     }
 }
 
-fn edit_list(index: usize, name: &str) {
+fn edit_list(index: usize, name: &[&str]) {
+    println!("called");
     let files = vector_files();
     let original = &files[index.clone()];
-    let output = fs::rename(original, name.to_owned() + EXTENSION);
+    let string = name.join(" ");
+    println!("{original}");
+    let output = fs::rename(original, (string + EXTENSION));
     match output {
         Ok(_) => println!("The list has been renamed successfully"),
         Err(e) => println!("An Error has occured \n\n{e}"),
